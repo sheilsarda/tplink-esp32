8d05bd4e81024156592d88073b3ed1991d193312
sheilsarda <sheilsarda@yahoo.com>
2022-01-05 12:17:02 -0500

    added softAP script

---
+++ softAPTest/softAPTest.ino
@@ -0,0 +1,24 @@
+#include <ESP8266WiFi.h>
+
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  Serial.print("Setting soft-AP configuration ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+
+  Serial.print("Setting soft-AP ... ");
+  Serial.println(WiFi.softAP("ESPsoftAP_01") ? "Ready" : "Failed!");
+
+  Serial.print("Soft-AP IP address = ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void loop() {
+Serial.printf("Stations connected to soft-AP = %d\n", WiFi.softAPgetStationNum());
+}
